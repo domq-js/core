e71da4f6f903b14d39abc410cb3ca9da5e11d866
Varun <varunsridharan23@gmail.com>
2020-09-12 19:05:47 +0530

    Added .html function.

--- src/attributes/index.js
+++ src/attributes/index.js
@@ -110,3 +110,18 @@ fn.toggleClass = function( cls, force ) {
 		} );
 	} );
 };
+
+fn.html = function( html ) {
+	if( !arguments.length ) {
+		return this[ 0 ] && this[ 0 ].innerHTML;
+	}
+	if( isUndefined( html ) ) {
+		return this;
+	}
+	return this.each( function( i, ele ) {
+		if( !isElement( ele ) ) {
+			return;
+		}
+		ele.innerHTML = html;
+	} );
+}
