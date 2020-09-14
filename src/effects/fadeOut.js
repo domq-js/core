60a6a148b492664a5a84038a1de9fd46bbdb5091
Varun <varunsridharan23@gmail.com>
2020-09-14 17:14:04 +0530

    Minor Update.

--- src/effects/fadeOut.js
+++ src/effects/fadeOut.js
@@ -1,5 +1,5 @@
 import { fn } from "../setup";
 
 fn.fadeOut = function( delay, easing, callback ) {
-	return this.fadeToggle( delay, easing, callback );
+	return this.fadeToggle( delay, easing, callback, false );
 };
