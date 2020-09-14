84523068956e22889ca545418eac62bb3764a998
Varun <varunsridharan23@gmail.com>
2020-09-14 17:09:27 +0530

    Updated RollUp Config.

--- rollup.config.js
+++ rollup.config.js
@@ -1,48 +1,38 @@
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
 import { uglify } from 'rollup-plugin-uglify';
 import filesize from 'rollup-plugin-filesize';
 import visualizer from 'rollup-plugin-visualizer';
 
-export default [
-	{
-		input: './src/wrap.js',
-		output: {
-			file: './dist/wpopv.min.js',
-			format: 'umd',
-			name: 'wpopv',
-		},
-		plugins: [
-			babel( {
-				exclude: 'node_modules/**'
-			} ),
-			resolve(),
-			commonjs(),
-			uglify( {
-				mangle: true,
-				output:{
-					braces:false,
-				}
-			} ),
-			filesize()
-		]
-	},
-	{
-		input: './src/wrap.js',
-		output: {
+
+export default {
+	input: './src/wrap.js',
+	output: [
+		{
 			file: './dist/wpopv.js',
 			format: 'umd',
 			name: 'wpopv',
 		},
-		plugins: [
-			babel( {
-				exclude: 'node_modules/**'
-			} ),
-			resolve(),
-			commonjs(),
-			filesize(),
-			visualizer()
-		]
-	},
-];
+		{
+			file: './dist/wpopv.min.js',
+			format: 'umd',
+			name: 'wpopv',
+			plugins: [
+				uglify( {
+					mangle: true,
+					output: {
+						braces: false,
+					}
+				} ),
+			]
+		}
+	],
+	plugins: [
+		babel( {
+			exclude: 'node_modules/**'
+		} ),
+		resolve(),
+		filesize(),
+		visualizer()
+	]
+};
