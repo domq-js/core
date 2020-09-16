173f81a91f7ac21c81c6d87a9525bc31efbacc04
Varun <varunsridharan23@gmail.com>
2020-09-16 06:48:24 +0530

    Added Merge.

---
+++ src/utilities/merge.js
@@ -0,0 +1,14 @@
+export default function( first, second ) {
+	var len = +second.length,
+		j   = 0,
+		i   = first.length;
+
+	for( ; j < len; j++ ) {
+		first[ i++ ] = second[ j ];
+	}
+
+	first.length = i;
+
+	return first;
+}
+
