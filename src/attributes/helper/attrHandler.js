7e987fb20eb13c61ef1eca790dfc6928247b37a5
Varun <varunsridharan23@gmail.com>
2020-09-17 07:20:55 +0530

    Minor Update.

--- src/attributes/helper/attrHandler.js
+++ src/attributes/helper/attrHandler.js
@@ -1,8 +1,12 @@
 import core from "../../setup";
 import isUndefined from "../../typechecking/isUndefined";
+import isElement from "../../typechecking/isElement";
 
 export default function( from, to, isMove = false ) {
 	return this.each( ( i, el ) => {
+		if( !isElement( el ) ) {
+			return;
+		}
 		let instance  = core( el );
 		let $existing = instance.attr( from );
 		if( !isUndefined( $existing ) ) {
