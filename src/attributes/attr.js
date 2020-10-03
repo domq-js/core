ba0345cde9a2f5a3b06ee9f15c80d14d3160385e
Varun <varunsridharan23@gmail.com>
2020-10-03 16:51:32 +0530

    Improved Attr Hook

--- src/attributes/attr.js
+++ src/attributes/attr.js
@@ -1,7 +1,7 @@
 import { fn } from "../setup";
 import { handleObjectDataLoop } from "../helper";
 import { isElement, isNull, isString, isUndefined } from "@varunsridharan/js-is";
-import attrHooks from "../hooks/attr";
+import { attrHook } from "../hooks/helper";
 
 fn.attr = function( name, value ) {
 	if( !name ) {
@@ -9,21 +9,15 @@ fn.attr = function( name, value ) {
 	}
 
 	if( isString( name ) ) {
-		let hook = ( !isUndefined( attrHooks[ name.toLowerCase() ] ) ) ? attrHooks[ name.toLowerCase() ] : false;
+		let ln = name.toLowerCase();
 		if( arguments.length < 2 ) {
 			if( !this[ 0 ] || !isElement( this[ 0 ] ) ) {
 				return;
 			}
 
-			let value = null;
-
-			if( hook && !isUndefined( hook.get ) ) {
-				value = hook.get( this[ 0 ], name );
-			}
-
-			if( isNull( value ) ) {
-				value = this[ 0 ].getAttribute( name );
-			}
+			let hook  = attrHook( ln, 'get' ),
+				value = ( hook ) ? hook( this[ 0 ], name ) : null;
+			value     = ( isNull( value ) ) ? this[ 0 ].getAttribute( name ) : value;
 			return isNull( value ) ? undefined : value;
 		}
 
@@ -35,12 +29,12 @@ fn.attr = function( name, value ) {
 			return this.removeAttr( name );
 		}
 
+		let hook = attrHook( ln, 'set' );
 		return this.each( ( i, ele ) => {
 			if( !isElement( ele ) ) {
 				return;
 			}
-
-			( hook && !isUndefined( hook.set ) ) ? hook.set( this[ 0 ], name, value ) : ele.setAttribute( name, value );
+			( hook ) ? hook( this[ 0 ], name, value ) : ele.setAttribute( name, value );
 		} );
 	}
 	handleObjectDataLoop.call( this, name, 'attr' );
---
+++ src/hooks/helper.js
@@ -0,0 +1,13 @@
+import { isUndefined } from "@varunsridharan/js-is";
+import core from "../setup";
+
+export function hookHandler( hookType, hookname, Type ) {
+	if( !isUndefined( core.hooks[ hookType ][ hookname ] ) && Type && !isUndefined( core.hooks[ hookType ][ hookname ][ Type ] ) ) {
+		return core.hooks[ hookType ][ hookname ][ Type ];
+	}
+	return false;
+}
+
+export function attrHook( hookName, type ) {
+	return hookHandler( 'attr', hookName, type );
+}
